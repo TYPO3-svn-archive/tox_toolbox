77f3f4c8e69d3d95182d8b2f156d160b98d91805
dasrick <dasrick@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-12-19 08:42:58 +0000

    add some settings around pageUnavailble
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tox_toolbox/trunk@15079 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "tox_toolbox"
 #
-# Auto generated 19-12-2008 02:09
+# Auto generated 19-12-2008 09:26
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'tox-Toolbox',
-	'description' => 'Some useful settings for Typo3-Backend. For Example: En/Disable extCache, adminOnly, pageUnavailable_force ...',
+	'description' => 'Some useful settings for Typo3-Backend. For Example: extCache, adminOnly, pageUnavailable_force and more. All these settings are available at install-tool too. Maybe it is useful if the install-tool is unavailable.',
 	'category' => 'be',
 	'author' => 'Enrico Hoffmann',
 	'author_email' => 'hoffmann.enrico@gmail.com',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.2',
+	'version' => '0.0.3',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"6c7a";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"8df5";s:12:"ext_icon.gif";s:4:"6c2b";s:17:"ext_localconf.php";s:4:"abb2";s:14:"ext_tables.php";s:4:"d2a7";s:14:"doc/manual.swx";s:4:"21af";}',
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"6c7a";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"7a85";s:12:"ext_icon.gif";s:4:"6c2b";s:17:"ext_localconf.php";s:4:"95fc";s:14:"ext_tables.php";s:4:"d2a7";s:14:"doc/manual.swx";s:4:"21af";}',
 	'suggests' => array(
 	),
 );
--- ext_localconf.php
+++ ext_localconf.php
@@ -8,6 +8,24 @@ if ($confArr['adminOnly']==1) {
 
 if ($confArr['pageUnavailable_force']==1) {
 	$TYPO3_CONF_VARS['FE']['pageUnavailable_force']=1;
+	
+	switch($confArr['pageUnavailable_handlingType']) {
+		case'readfile':
+			$fString=trim($confArr['pageUnavailable_handlingString']);
+			$hString=(strlen($fString)>0)?'READFILE:'.$fString:'';
+			break;
+			
+		case'redirect':
+			$fString=trim($confArr['pageUnavailable_handlingString']);
+			$hString=(strlen($fString)>0)?'REDIRECT:'.$fString:'';
+			break;
+			
+		case'default':
+		default:
+			$hString='';
+			break;
+	}
+	$TYPO3_CONF_VARS['FE']['pageUnavailable_handling']=$hString;
 }
 
 if ($confArr['extCache']==1) {
